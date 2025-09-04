262d4b141f1cd8a05cc5d2d94d12b6d85c2952b6
Fred Nora <frednora@outlook.com>
2025-09-04 14:47:01 -0300

    bash: Learning the basics.

--- test.sh
+++ test.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
+VERSION="1.0.0"
+
 echo "This is a test"
 
-if [ $1 ] then
-	echo "Version"
+if [ "$1" == "-v" ] || [ "$1" == "--version" ]; then
+    echo "Version: $VERSION"
 fi
-
-
-
-
-
+echo "Script completed."
