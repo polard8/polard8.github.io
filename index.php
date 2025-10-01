df713e2b59de8de5fd2f11c35571f40e21e4164e
Fred Nora <frednora@outlook.com>
2025-10-01 10:00:09 -0300

    Seek balance: Move serach.php to root

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php 
 define('USE_XXX', true);
 // ...
-require ('./search/search.php');
+require ('./search.php');
 // require ('./search/search02.php');
 //require('./sb.php');
 //require('./sb02.php');
